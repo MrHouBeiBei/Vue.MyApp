a462849de6bbb4f8eb034f53d1a14f1bdcb2c658
Herbie <843834089@qq.com>
2018-01-21 00:30:59 +0800

    关闭cli严格模式&&子组件通过watch父组件变量触发事件测试

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
+// Vue.config.silent = true
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/test.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+    <watch-test :oneString="boyA" @girlToBoy="receiveLove"></watch-test>
+    
+    <button @click="changboyA">改变父组件变量</button>
+  </div>
+</template>
+
+<script>
+import watchTest from '../components/watchTest.vue'
+
+export default {
+  name:'test',
+  components: {
+    'watch-test': watchTest
+  },
+  data () {
+    return {
+      msg: '测试页面',
+      boyA: 1
+    }
+  },
+  methods: {
+    changboyA() {
+      this.boyA += 1
+      console.log(this.boyA)
+    },
+    receiveLove(data) {
+      console.log('父组件响应子组件事件', data)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
