a715cc70b549a3d47c9106a80c45402f770b36be
Herbie <843834089@qq.com>
2018-05-14 14:16:14 +0800

    feat: data测试优化&&commit提交规范

--- src/pages/data/index.vue
+++ src/pages/data/index.vue
@@ -3,19 +3,27 @@
       <input type="button" value="按钮a" @click="aClick">
       <p>{{a}}</p>
       <br>
+
       <input type="button" value="按钮b" @click="bClick">
       <p v-if="form.b">{{form.b}}</p>
       <br>
+
       <input type="button" value="按钮b2" @click="b2Click">
       <p v-if="form.b2">{{form.b2}}</p>
+      <p v-if="form.bb">{{form.bb}}</p>
       <br>
+
       <input type="button" value="按钮c" @click="cClick">
       <p v-if="form.c">{{form.c}}</p>
       <br>
+
       <input type="button" value="按钮d" @click="dClick">
       <p v-if="form.d&&form.d.e">{{form.d.e}}</p>
       <br>
+      
        <input type="button" value="按钮list" @click="listClick">
+       <input type="button" value="按钮修改list" @click="listClick2">
+       <input type="button" value="按钮传统添加list" @click="listClick3">
       <ul v-if="form.children&&form.children.length > 0">
           <li v-for="(item, index) of form.children" :key="index">{{item.value}}</li>
       </ul>
@@ -47,12 +55,13 @@ export default {
           this.a += 2
       },
       bClick() {
-        //   this.form.b = 'b'     //不响应  data中未定义
-        //   this.form.b2 = 'b2'   //响应  data中定义
-        this.form = Object.assign({}, this.form, {b: 'b'})   //响应
+          this.form.b = 'b'     //不响应  data中未定义
+        // this.form = Object.assign({}, this.form, {b: 'b'})   //响应
+
       },
       b2Click() {
-          this.form.b2 = 'b2'
+          this.form.b2 = 'b2'    //响应  data中定义
+        //   this.form.bb = 'bb'    //本身为非响应  
       },
       cClick() {
           this.$set(this.form, 'c', 'c')
@@ -63,6 +72,17 @@ export default {
       },
       listClick() {
           this.form.children.push({value: 'a'})
+      },
+      listClick2() {
+          this.form.children[0].value = 'b'
+      },
+      listClick3() {
+          /**
+           * 之后执行 listClick 事件 可触动试图更新在页面显示出来，但还是非响应式属性，不可被 listClick2   事件修改
+           * 
+           */
+          this.form.children[this.form.children.length] = {value: 'aa'}  
+        //   this.$forceUpdate()  //可更新非响应数据
       }
   }
 }
